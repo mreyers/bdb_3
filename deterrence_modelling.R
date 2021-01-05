04ea7bf427ef5cae5aaeaeb20c22bcb608632c65
Meyappan <meysubb@gmail.com>
2021-01-05 10:07:48 -0500

    add notes and clean up valuation script

--- deterrence_modelling.R
+++ deterrence_modelling.R
@@ -5,22 +5,120 @@ library(lubridate)
 
 final_train_df <- readRDS("final_train_deterrence.RDS") %>% 
   mutate(closest_defensive_player_f = as.factor(closest_defensive_player_f))
+
+# From lucas assignments
+def_assignments <- readRDS("data/def_assignment_df.rds")
+
+target <<-
+  read_csv("data/targetedReceiver.csv")  %>% janitor::clean_names()
+
 players_name <- read_csv("data/players.csv") %>% mutate(nflId=as.character(nflId)) 
 
-end_of_18 <- lubridate::mdy(12312018)
+## calculate rough age for players,to create `is_young` variable
+# end_of_18 <- lubridate::mdy(12312018)
+# 
+# players_name = players_name %>% mutate(nflId=as.character(nflId)) %>%
+#   mutate(birth_date = as.Date(birthDate),
+#          age = interval(birth_date,end_of_18)/years(1),
+#          is_young = age < 22.5)
+# 
+# young_players <- players_name %>% select(nflId,is_young) %>% mutate(nfl_id = as.factor(nflId))
 
-players_name = players_name %>% mutate(nflId=as.character(nflId)) %>%
-  mutate(birth_date = as.Date(birthDate),
-         age = interval(birth_date,end_of_18)/years(1),
-         is_young = age < 22.5)
+# final_train_df <- final_train_df %>% left_join(young_players,by=c("closest_defensive_player_f"="nfl_id")) %>% 
+#   mutate(
+#     logit_cp = logit_scaled(cp)
+#   )
 
-young_players <- players_name %>% select(nflId,is_young) %>% mutate(nfl_id = as.factor(nflId))
+create_factor <- function(col) {
+  cnt <- table(col)
+  mid_value = quantile(cnt)[3]
+  keep_ids <- names(cnt[cnt > mid_value])
+  return(keep_ids)
+}
 
-final_train_df <- final_train_df %>% left_join(young_players,by=c("closest_defensive_player_f"="nfl_id")) %>% 
+test = final_train_df %>% select(play_id,game_id,nfl_id,cp) %>% distinct()
+
+final_train_df2 <- test  %>% 
+  left_join(def_assignments,by=c("game_id","play_id","nfl_id"="assigned_rec_id")) %>%
+  filter(!is.na(nearest_rec_id)) %>%
+  left_join(target) %>%
   mutate(
-    logit_cp = logit_scaled(cp)
+    assigned_rec_id = nfl_id,
+    closest_defensive_player_f = ifelse(
+      defender_id %in% create_factor(defender_id),
+      defender_id,
+      999999
+    ),
+    # set different value if player is wide open
+    closest_defensive_player_f = ifelse(
+      is.na(defender_id),
+      999998,
+      closest_defensive_player_f
+    ),
+    closest_offensive_player_f = as.factor(if_else(
+      assigned_rec_id %in% create_factor(assigned_rec_id), assigned_rec_id, 999999
+    )),
+    logit_cp = logit_scaled(cp),
+    target = (assigned_rec_id == target_nfl_id),
+    target = if_else(is.na(target),
+                   FALSE,
+                   target)
   )
 
+saveRDS(final_train_df2,"final_train_df_lucas.RDS")
+
+target_model_cp <-
+  brm(
+    target ~ logit_cp +
+      (1 | closest_defensive_player_f) +
+      (1 | closest_offensive_player_f),
+    data = final_train_df2,
+    family = bernoulli(link = "logit"),
+    cores = 4
+  )
+
+ saveRDS(target_model_cp,"cp_det_model_lucas.RDS")
+
+ran_eff_df <- ranef(target_model_cp)
+
+wr <-
+  ran_eff_df$closest_offensive_player_f %>% as.data.frame %>%
+  tibble::rownames_to_column(var ="nflId") %>%
+  left_join(players_name)
+
+def <-
+  ran_eff_df$closest_defensive_player_f %>% as.data.frame %>%
+  tibble::rownames_to_column(var ="nflId") %>%
+  left_join(players_name)
+
+
+## cp model by player REs
+## used to calculate average defender xcp
+
+exp_cp_player_model <- brm(
+  logit_cp ~ + 
+    (1 | closest_defensive_player_f) +
+    (1 | closest_offensive_player_f),
+  data = final_train_df2,
+  family = gaussian(),
+  cores = 4
+)
+
+ran_eff_df <- ranef(exp_cp_player_model)
+
+wr <-
+  ran_eff_df$closest_offensive_player_f %>% as.data.frame %>%
+  tibble::rownames_to_column(var ="nflId") %>%
+  left_join(players_name)
+
+def <-
+  ran_eff_df$closest_defensive_player_f %>% as.data.frame %>%
+  tibble::rownames_to_column(var ="nflId") %>%
+  left_join(players_name)
+
+saveRDS(exp_cp_player_model,"exp_cp_model.RDS")
+
+### Model Types
 ## scale the data. 
 ## see how this helps
 # basic_fe <- glm(target ~ down + distance + air_dist,
@@ -147,35 +245,8 @@ final_train_df <- final_train_df %>% left_join(young_players,by=c("closest_defen
 # 
 # saveRDS(final_binomial_model,"final_det_model.RDS")
 
-target_model_cp <-
-  brm(
-    target ~ logit_cp +
-      (1 | closest_defensive_player_f) +
-      (1 | closest_offensive_player_f),
-    data = final_train_df,
-    family = bernoulli(link = "logit"),
-    cores = 4
-  )
 
-saveRDS(target_model_cp,"cp_det_model.RDS")
-
-ran_eff_df <- ranef(deterrence_model)
-
-wr <-
-  ran_eff_df$closest_offensive_player_f %>% as.data.frame %>%
-  tibble::rownames_to_column(var ="nflId") %>%
-  left_join(players_name)
-
-def <-
-  ran_eff_df$closest_defensive_player_f %>% as.data.frame %>%
-  tibble::rownames_to_column(var ="nflId") %>%
-  left_join(players_name)
-
-
-
-
-
-# ### week 1 plays with coverage, look to see if preds change with coverage
+# week 1 plays with coverage, look to see if preds change with coverage
 # 
 # cov_week1 <- read_csv("data/coverages_week1.csv") %>% janitor::clean_names()
 # week_1_df <- final_train_df %>% inner_join(cov_week1)
--- deterrence_valuation.R
+++ deterrence_valuation.R
@@ -1,9 +1,26 @@
 library(tidyverse)
 library(brms)
+## clean this code up and create two steps
 
-final_train_df <- readRDS("final_train_deterrence.RDS")
+
+### One Method
+### 1 - Calculate P(target) w/OFf + Def RE
+### 2 - Calculate xCP with average Defender + Use avg cCP as input to P(target) w/Off + AVG Def RE
+### 3 - RE Diff = P(target) - P(target_avg_re) 
+### 4 - Valuation = RE Diff * Hypothetical EPA
+
+soft_max_norm <- function(prob_vector){
+  exp_vec_values = exp(prob_vector)
+  norm_values = exp_vec_values/sum(exp_vec_values)
+  return(norm_values)
+}
+
+frame_df <- readRDS("final_train_deterrence.RDS")
+frame_df2 <- frame_df %>% select(play_id,game_id,frame_id) %>% distinct()
+
+final_train_df <- readRDS("final_train_df_lucas.RDS")
 hypothetical_df <- readRDS("data/first_pass_ep.rds")
-deterrence_model <- readRDS("cp_det_model.RDS")
+deterrence_model <- readRDS("cp_det_model_lucas.RDS")
   
 
 hyp_epa <- hypothetical_df %>% select(game_id,play_id,
@@ -12,29 +29,40 @@ hyp_epa <- hypothetical_df %>% select(game_id,play_id,
                                       complete_epa,incomplete_epa) %>% 
   mutate(hyp_epa = .pred_C * complete_epa + .pred_I * incomplete_epa)
 
-combine_epa_df <- final_train_df %>% inner_join(hyp_epa) %>%
+combine_epa_df <- final_train_df %>% left_join(frame_df2) %>% inner_join(hyp_epa) %>%
   mutate(
     logit_cp = logit_scaled(cp)
   )
 
-## ask the gorup why its asking for new levels
-## when the level already exists
-target_preds_no_re <- predict(deterrence_model,newdata = combine_epa_df,
-                        re_formula = ~ (1 | closest_offensive_player_f),
-                        allow_new_levels = T) %>% as.data.frame()
-colnames(target_preds_no_re) <- paste0(colnames(target_preds_no_re),"_no_re")
+## 
+unique_offensive_players <- final_train_df %>% 
+  select(closest_offensive_player_f) %>% distinct() %>%
+  mutate(
+    closest_defensive_player_f = 999999
+  )
 
-target_preds_re <- predict(deterrence_model,newdata = combine_epa_df,
+exp_cp_player_model <- readRDS("exp_cp_model.RDS")
+
+average_defender <- predict(exp_cp_player_model,newdata = unique_offensive_players) %>% as.data.frame()
+
+average_cp_df <- bind_cols(unique_offensive_players,average_defender)
+
+re_pred_input <- combine_epa_df %>% select(logit_cp,closest_defensive_player_f,closest_offensive_player_f)
+
+no_re_pred_input <- combine_epa_df %>% select(closest_offensive_player_f) %>%
+  left_join(average_cp_df) %>%
+  rename(logit_cp = Estimate)
+
+
+target_preds_re <- predict(deterrence_model,newdata = re_pred_input,
                            allow_new_levels = T) %>% as.data.frame()
 colnames(target_preds_re) <- paste0(colnames(target_preds_re),"_re")
 
-target_preds <- bind_cols(target_preds_no_re,target_preds_re) 
+target_preds_no_re <- predict(deterrence_model,newdata = no_re_pred_input,
+                              allow_new_levels = T) %>% as.data.frame()
+colnames(target_preds_no_re) <- paste0(colnames(target_preds_no_re),"_no_re")
 
-soft_max_norm <- function(prob_vector){
- exp_vec_values = exp(prob_vector)
- norm_values = exp_vec_values/sum(exp_vec_values)
- return(norm_values)
-}
+target_preds <- bind_cols(target_preds_no_re,target_preds_re) 
 
 
 combine_df <- bind_cols(combine_epa_df,target_preds) %>% 
@@ -44,10 +72,43 @@ combine_df <- bind_cols(combine_epa_df,target_preds) %>%
     norm_no_re_estimate = soft_max_norm(Estimate_no_re),
     re_diff = norm_re_estimate - norm_no_re_estimate)
 
-## i need to drop the defender random effect
-## offensive player, instead of the average player 
+saveRDS(combine_df,"combine_df.rds")
 
-deterence_value <- combine_df %>% group_by(game_id, play_id) %>%
+  
+deterence_value <- combine_df %>% group_by(game_id,play_id) %>%
+  mutate(
+  modified = if_else(target==1,1,-1),
+  deterrence_value = modified * hyp_epa * norm_no_re_estimate,
+  deterrence_over_expected =  hyp_epa * re_diff
+) %>% group_by(defender_id) %>%
+  summarise(
+    total_plays = n(),
+    total_epa_value = sum(deterrence_value,na.rm=T),
+    total_epa_value_over_expected = sum(deterrence_over_expected,na.rm=T)
+  ) #%>% filter(closest_defensive_player!=999999)
+
+saveRDS(deterence_value,"deterrence_unsummary.RDS")
+
+### Second Method
+### 1 - Calculate xCP with average Defender + Use avg cCP as input to P(target) w/Off + AVG Def RE 
+### 2 - Option to use P(target) w/Off RE only
+### Valuation
+### when targeted, be penalized if the player you  are covering is NOT the most valuable target on the field by the magnitude of the difference
+### when not targeted, be rewarded if your target is MORE valuable than the selected target
+
+#hypothetical_df <- readRDS("data/first_pass_ep.rds")
+#final_train_df <- readRDS("final_train_deterrence.RDS")
+#deterrence_model <- readRDS("cp_det_model.RDS")
+
+## Optional method to calculate with no RE
+# target_preds_no_re <- predict(deterrence_model,newdata = combine_epa_df,
+#                         re_formula = ~ (1 | closest_offensive_player_f),
+#                         allow_new_levels = T) %>% as.data.frame()
+# colnames(target_preds_no_re) <- paste0(colnames(target_preds_no_re),"_no_re")
+
+
+
+deterence_value2 <- combine_df %>% group_by(game_id, play_id) %>%
   mutate(
     observed_hyp_epa = max(target * hyp_epa),
     max_hyp_epa = max(hyp_epa),
@@ -58,33 +119,19 @@ deterence_value <- combine_df %>% group_by(game_id, play_id) %>%
     second_term = (max_hyp_epa - observed_hyp_epa) * target * (1 - norm_no_re_estimate),
     deterrence_estimate = first_term + second_term
   ) %>%
-  group_by(closest_defensive_player) %>%
+  group_by(defender_id) %>%
   summarize(total_det_value = sum(deterrence_estimate)) %>%
   arrange(total_det_value) 
-  
-  
-#   old method to value players
-#   mutate(
-#   modified = if_else(target==1,1,-1),
-#   deterrence_value = modified * hyp_epa * norm_no_re_estimate,
-#   deterrence_over_expected =  hyp_epa * re_diff
-# ) %>% group_by(closest_defensive_player) %>% 
-#   summarise(
-#     total_plays = n(),
-#     total_epa_value = sum(deterrence_value,na.rm=T),
-#     total_epa_value_over_expected = sum(deterrence_over_expected,na.rm=T)
-#   ) %>% filter(closest_defensive_player!=999999) 
 
-saveRDS(deterence_value,"deterrence_unsummary.RDS")
 
-## players df 
+## Join data with names
 players_df <-
   read_csv("data/players.csv") %>% janitor::clean_names() %>% mutate(nfl_id = as.factor(nfl_id))
 
-det_df <- deterence_value %>%
-  rename( nfl_id=closest_defensive_player) %>%
+det_df <- deterence_value2 %>%
+  rename( nfl_id=defender_id) %>%
   mutate(nfl_id = as.factor(nfl_id)) %>%
   left_join(players_df) 
 
 
-saveRDS(det_df,"deterrence_summary.RDS")
+saveRDS(det_df,"deterrence_summary_update.RDS")
